7b1e9658c9b83ae40c9ecd56c67ec3834b41c7c6
skandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2022-10-24 13:43:17 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -36,3 +36,21 @@ int main(){
         cout<<endl;
     } 
 }
+
+//same as the aboove pattern but it prints n instead of stars
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows"<<endl;
+    cin>>n;
+    
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout<<n;
+        }
+        cout<<endl;
+    } 
+}
