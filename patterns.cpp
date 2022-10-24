e8fee4f18745c2feb77ee0c23fb02aae81b5efac
skandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2022-10-24 13:39:39 +0530

    Create patterns.cpp

---
+++ patterns.cpp
@@ -0,0 +1,38 @@
+// This partucualar pattern is quiet simple . it printsout a square of stars with side equal to n
+
+//using while loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows"<<endl;
+    cin>>n;
+    int i = 1;
+    while(i <= n){
+        for(int j = 0; j < n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        i++;
+    } 
+}
+
+//using only for loops
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows"<<endl;
+    cin>>n;
+    
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    } 
+}
