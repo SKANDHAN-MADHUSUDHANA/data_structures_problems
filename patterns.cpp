764a9775ba0d9867ea119f6965225d87befa7043
skandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2022-10-24 22:48:11 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -585,3 +585,37 @@ int main(){
     cout<<endl;
     }
 }
+
+//The pattern is
+/*
+   *   
+  ***
+ *****
+*******
+
+for n = 4;
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    
+    for (int i = 1; i <= n; i++){
+        int p = 2*i - 1;
+        int m = 1;
+        for(int j = 1; j <= 2*n - 1; j++){
+            for(int k = 1; k <= n - i; k++){
+                cout<<" ";
+            }
+            while(m<=p){
+                cout<<"*";
+                m++;
+            }
+        }
+        cout<<endl;
+    }
+}
