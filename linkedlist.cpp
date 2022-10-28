5a7f68dabaa0882b1dd7705c05439c25fba0c754
skandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2022-10-28 13:57:39 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -723,3 +723,80 @@ int main(){
     head = deleteRecursively(head,key);
     print(head);
 }
+
+
+//finding the mid point of the linked list without finding the length of the linked list
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node * next;
+        
+    Node(int data){
+        this -> data = data;
+        next = NULL;
+    }
+};
+
+
+Node * takeInput(){
+    int data;
+    cout<<"Enter the data of the node to be inserted ( use -1 to terminate the insertion ) : ";
+    cin>>data;
+    Node * head = NULL;
+    Node * tail = NULL;
+    
+    while(data != -1){
+        Node * newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail = tail -> next;
+        }
+    cout<<"Enter the data of the node to be inserted ( use -1 to terminate the insertion ) : ";
+    cin>>data;
+    }
+    
+    return head;
+}
+
+void print(Node * head){
+    if(head == NULL){
+        cout<<"The linked list id empty !"<<endl;
+        return;
+    }
+    
+    Node * temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int returnMID(Node * head){
+    Node * slow = head;
+    Node * fast = head;
+    if(head != NULL){
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    }
+    return slow->data;
+}
+
+int main(){
+    int  key;
+    Node * head = takeInput();
+    print(head);
+    int ans = returnMID(head);
+    cout<<"The mid point of the linked list is : "<<ans<<endl;
+    
+}
