d90795897c84363bad3800034225a30a97981b74
skandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2022-10-28 10:15:34 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -563,3 +563,82 @@ int main(){
     print(head);
 }
 
+///inserting at postittion recursively
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node * next;
+        
+    Node(int data){
+        this -> data = data;
+        next = NULL;
+    }
+};
+
+
+Node * takeInput(){
+    int data;
+    cout<<"Enter the data of the node to be inserted ( use -1 to terminate the insertion ) : ";
+    cin>>data;
+    Node * head = NULL;
+    Node * tail = NULL;
+    
+    while(data != -1){
+        Node * newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail = tail -> next;
+        }
+    cout<<"Enter the data of the node to be inserted ( use -1 to terminate the insertion ) : ";
+    cin>>data;
+    }
+    
+    return head;
+}
+
+void print(Node * head){
+    if(head == NULL){
+        cout<<"The linked list id empty !"<<endl;
+        return;
+    }
+    
+    Node * temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+Node * insertRecursive(Node * head, int data , int key){
+    if (head == NULL || key == 0){ // this condition helps in handling insetion at a position greater then length of ll by inserting at end
+        Node * newNode = new Node(data);
+        newNode -> next = head;
+        head = newNode;
+    }
+    else{
+        head -> next = insertRecursive(head -> next , data , key - 1); //breaking down of the problem into smaller problem 
+    }
+    
+    return head;
+}
+
+int main(){
+    int data, key;
+    Node * head = takeInput();
+    print(head);
+    cout<<"Enter the data of the node to be inserted : ";
+    cin>>data;
+    cout<<"Enter the position of insertion : ";
+    cin>>key;
+    head = insertRecursive(head,data,key);
+    print(head);
+}
